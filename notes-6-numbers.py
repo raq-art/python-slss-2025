4c4fefd93198296c221fff67ace845ebc1d5d1ac
R yip <yipr1030@gmail.com>
2025-11-05 13:05:26 -0800

    added numbers notes

---
+++ notes-6-numbers.py
@@ -0,0 +1,20 @@
+# Numbers and Operations
+# Author: Rachel
+#  5 November 2025
+
+# Create an algorithm to gather
+# data to find the most popular
+# bubble ttea place around us
+
+# Version 1
+# Show all the Bbt choices
+# Ask the user for their choice
+# Add their vote to a running tally
+# Give some raw scores
+# give score as a percentage
+
+# Version 2
+# ask the user what their favorite bbt place
+# Add their vote to a running tally
+# Give the raw scores
+# give score as a perentage
