9821211924dfa34bdaec2ce9b6a809190f8d3a07
R yip <yipr1030@gmail.com>
2025-12-09 11:58:19 -0800

    added notes 8

--- notes-8-search.py
+++ notes-8-search.py
@@ -6,7 +6,6 @@ import csv
 
 
 # Introduction to search algorithms
-# Search for all songs for "Kendrick"
 # Display all youtube and tiktok views
 def main():
     # track name - 0
@@ -17,9 +16,9 @@ def main():
     artist_col = 2
     yt_views_col = 11
     tiktok_views_col = 15
-    artist = "Kendrick Lamar"
+    artist = "Sabrina Carpenter"
 
-    kendrick_songs = []
+    Sabrina_songs = []
 
     # open the file
     with open("data/spotify2024.csv") as f:
@@ -28,11 +27,30 @@ def main():
         # create a reader object
         r = csv.reader(f)
 
+        # read each line of data
         for info in r:
-            # if the current artist is "Kendrick"
             if info[artist_col] == artist:
-                kendrick_songs.append(info)
-        print(f"Number of Kendrick Songs: {len(kendrick_songs)}")
+                Sabrina_songs.append(info)
+        print(f"Number of Sabrina Songs: {len(Sabrina_songs)}")
+
+        for song in Sabrina_songs:
+            current_track_name = song[song_name_col]
+            current_yt_views = song[yt_views_col]
+            current_tiktok_views = song[tiktok_views_col]
+            # display information in a clear way
+            # Squabble Up       100,000,000       120,000,000
+            if 11 < len(current_track_name) > 15:
+                tabs = "\t\t"
+            elif 13 < len(current_track_name):
+                num_tabs = 1
+            else:
+                num_tabs = 3
+
+                print(
+                    f"{current_track_name.strip()}{'\t' * num_tabs}{current_yt_views.strip()}{'\t' * num_tabs}{current_tiktok_views.strip()}"
+                )
+
+            # display info in a clear way
 
     #
     # read each line
